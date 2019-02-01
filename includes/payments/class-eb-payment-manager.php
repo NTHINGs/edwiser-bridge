456aa28c89fb7876849eb62a527fc6846b97c167
Mauricio Martinez <n_othing@hotmail.com>
2019-01-31 19:36:58 -0600

    Testing

--- includes/payments/class-eb-payment-manager.php
+++ includes/payments/class-eb-payment-manager.php
@@ -275,7 +275,7 @@ class EBPaymentManager
                     <button type="button" class="btn btn-default" data-toggle="modal" data-target="#comprar">
                         Comprar Curso
                     </button>
-                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal fade" id="comprar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                             <div class="modal-header">
