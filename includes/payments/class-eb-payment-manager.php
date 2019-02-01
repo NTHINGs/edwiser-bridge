758ae3401f6d3cc05f4ab33977c00a757fc652f5
Mauricio Martinez <n_othing@hotmail.com>
2019-02-01 15:31:23 -0600

    Removing test

--- includes/payments/class-eb-payment-manager.php
+++ includes/payments/class-eb-payment-manager.php
@@ -271,30 +271,7 @@ class EBPaymentManager
 
                 $paypal_button = '';
                 if (!empty($paypal_email)) {
-                    ?>
-                    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#comprar">
-                        Comprar Curso
-                    </button>
-                    <div class="modal fade" id="comprar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                ...
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
-                    <?php
+                    
                     $paypal_button = wptexturize(
                         do_shortcode(
                             "[paypal type='paynow'
